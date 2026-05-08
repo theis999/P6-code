4a0a5e58fa24d88d58c714d7594792548d4a1846
Head9x <git@caf-tranberg.dk>
2026-05-08 13:37:31 +0200

    TokenDebug test v2 - missing directive

--- newnewWebinterface/Pages/TokenDebug.cshtml.cs
+++ newnewWebinterface/Pages/TokenDebug.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace RazorPageOidc.Pages;
 
