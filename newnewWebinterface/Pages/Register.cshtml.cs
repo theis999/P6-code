4ebcaaf92d159d5e3f64542adfe9e2ccda3b64dc
Morten <morten.s.olesen@gmail.com>
2026-05-08 13:46:27 +0200

    Added Register Page

---
+++ newnewWebinterface/Pages/Register.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace newnewWebinterface.Pages;
+
+[Authorize]
+public class RegisterModel : PageModel
+{
+    public string name;
+    public string ProductID;
+
+    string authentikUserID = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        
+        var content = new JsonObject
+        {
+            ["name"] = name,
+            ["ProductID"] = ProductID,
+            ["authentikUserID"] = authentikUserID
+        };
+
+        await client.PostAsync("smakdb.head9x.dk/boards", content);
+
+    }
+}
