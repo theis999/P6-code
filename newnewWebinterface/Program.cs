df7d054e415d945c1a7242803eb1c6c4307092c2
Morten <morten.s.olesen@gmail.com>
2026-05-12 10:30:45 +0200

    CORS maybe

--- newnewWebinterface/Program.cs
+++ newnewWebinterface/Program.cs
@@ -11,7 +11,19 @@ using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.AspNetCore.HttpOverrides;
 using System.Net;
 
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("https://smakanalysis.head9x.dk",
+                                              "https://smakauth.head9x.dk");
+                      });
+});
 
 
 builder.Services.AddAuthentication(options =>
@@ -135,6 +147,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthentication();
 app.UseAuthorization();
